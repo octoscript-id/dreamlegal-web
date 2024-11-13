45a9909ee6c1a6b271b26dc3264d4fb1cf838ca8
alfadlimaulana <alfadlimaulana@gmail.com>
2024-11-13 08:44:39 +0700

    refactor: update benefit section ui

--- src/components/benefit/BenefitCard.tsx
+++ src/components/benefit/BenefitCard.tsx
@@ -1,19 +1,37 @@
-import Image from "next/image";
-import { Badge } from "../ui/badge";
 import { Benefit } from "@/types/Benefit";
+import { AlarmClockCheck, LayoutGrid, ListChecks, ShieldCheck, Target } from "lucide-react";
 
 interface BenefitCardProps {
     benefit: Benefit
 }
 
+const getIcon = (iconName: string) => {
+    switch (iconName) {
+        case 'listChecks':
+            return <ListChecks size={24} className="text-brand-800"/>
+        case 'alarmClockCheck':
+            return <AlarmClockCheck size={24} className="text-brand-800"/>
+        case 'target':
+            return <Target size={24} className="text-brand-800"/>
+        case 'layoutGrid':
+            return <LayoutGrid size={24} className="text-brand-800"/>
+        case 'shieldCheck':
+            return <ShieldCheck size={24} className="text-brand-800"/>
+        default:
+            break;
+    }
+}
+
 export default function BenefitCard({benefit}: BenefitCardProps) {
     return (
-        <div className={`p-4 space-y-6 md:space-y-4 border-l border-brand-100`}>
-            <Badge variant="blue" className="font-medium">{benefit.benefit}</Badge>
-            <div className={`max-md:hidden w-full relative h-[200px] xl:h-[272px]`}>
-                <Image src={`/img/${benefit.image}`} fill alt="step-1" className="object-cover rounded-xl"/>
+        <div className={`p-4 w-full md:w-[calc(50%-16px)] xl:w-[calc(34%-32px)] md:p-6 xl:p-8 flex items-start gap-6 border border-brand-100 rounded-xl`}>
+            <div className="bg-brand-50 rounded-full p-4 grid place-items-center">
+                {getIcon(benefit.icon)}
             </div>
-            <p className="text-gray-700 text-sm xl:text-base">{benefit.description}</p>
+            <div className="space-y-1">
+                <h3 className="text-lg font-medium">{benefit.benefit}</h3>
+                <p className="text-gray-700 text-base">{benefit.description}</p>
+            </div>
         </div>
     );
 }
