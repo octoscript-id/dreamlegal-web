75e36a520f54b6f255133d7c2206a4fb2c79eae3
WafiFz <wafifahruz@gmail.com>
2024-11-15 10:27:46 +0700

    style: update button UI, optimize HeroSection layout

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -8,16 +8,16 @@ import { Button } from "./ui/button";
 
 export default function HeroSection() {
     return (
-        <section id="hero-banner" className="pt-6 xl:pt-12 pb-12 md:pb-14 xl:pb-[72px] flex flex-col gap-10 items-center md:items-start lg:flex-row lg:justify-center w-full">
-            <section className="flex flex-col justify-between max-w-md">
-                <section className="flex flex-col items-center md:items-start gap-4">
+        <section id="hero-banner" className="xl:pt-12 pb-12 md:pb-14 xl:pb-[72px] flex flex-col gap-10 items-center md:items-start lg:flex-row lg:justify-center w-full">
+            <section className="flex flex-col justify-between max-w-xl">
+                <section className="flex flex-col items-center md:items-start gap-2">
                     <Badge variant="blue" className="w-fit">
                         Dream Legal
                     </Badge>
-                    <h1 className="font-display text-display-sm md:text-display-md lg:text-display-lg text-center md:text-left">
+                    <h1 className="font-display text-[32px] md:text-display-md lg:text-display-lg text-center md:text-left">
                         <span className="text-brand-700">Legal Partner</span> For Your Business
                     </h1>
-                    <p className="text-body-xl text-center md:text-left">
+                    <p className="text-[16px] md:text-body-xl text-center md:text-left">
                         Start your business with Dream Legal -- Penyedia jasa legal online untuk membantu Anda memulai bisnis di Indonesia.
                     </p>
                     <Button onClick={() => sendWhatsappMessage(CONTACT_NUMBER, CONTACT_MESSAGE)} className="w-fit hidden md:block">
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-base font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded text-base font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
@@ -24,7 +24,7 @@ const buttonVariants = cva(
       size: {
         default: "px-6 py-4",
         sm: "px-6 py-4 text-xs",
-        lg: "h-10 rounded-md px-8",
+        lg: "h-10 rounded-lg px-8",
         icon: "h-9 w-9",
       },
     },
@@ -34,6 +34,7 @@ const buttonVariants = cva(
     },
   }
 )
+
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
