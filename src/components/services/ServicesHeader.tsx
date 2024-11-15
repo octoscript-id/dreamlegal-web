bf780d97df05fa8ea1359e01e5a4b80af7cc1f36
WafiFz <wafifahruz@gmail.com>
2024-11-15 10:39:48 +0700

    docs: modify ServicesHeader copy

--- src/components/services/ServicesHeader.tsx
+++ src/components/services/ServicesHeader.tsx
@@ -13,7 +13,7 @@ export default function ServicesHeader() {
         <div className="text-center md:text-start max-xl:space-y-8 xl:flex items-end">
             <div className="space-y-2 xl:space-y-4 w-full">
                 <h1 className="text-[28px] md:text-[32px] xl:text-[40px] leading-9">Layanan Kami</h1>
-                <p className="text-gray-700 leading-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <p className="text-gray-700 leading-6">Kami menyediakan layanan konsultasi dan pendampingan hukum yang profesional dan komprehensif.</p>
             </div>
             <div className={`flex gap-4 max-md:justify-center`}>
                 <Button variant={pathname == "/" ? "tertiary" : "default"} onClick={() => window.open(SERVICE_BROCHURE_URL, "_blank")} className={`max-xl:text-sm ${pathname == "/" && "max-md:rounded-full"}`}>
